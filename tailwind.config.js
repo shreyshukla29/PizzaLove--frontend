9cc8905f0bf1275dd1e1e34a85face05b39eb88f
shrey <shreyshukla101217@gmail.com>
2024-06-24 17:58:33 +0530

    home

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, TSX files in src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
